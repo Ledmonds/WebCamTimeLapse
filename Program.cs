48fd8c8c9d650dede84fe88ce1f6ead2e6b1d243
Leuan Edmonds <leuan.edmonds@gmail.com>
2021-05-10 07:52:57 +0200

    Initial Code Push

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Emgu.CV;
+using System;
+using System.Drawing;
+
+namespace WebCamTimeLapse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            VideoCapture capture = new VideoCapture(); //create a camera capture
+            var image = capture.QueryFrame(); //take a picture
+
+            image.Save(@"C:\output.image.png");
+        }
+    }
+}
